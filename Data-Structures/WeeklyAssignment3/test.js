bb41ae3d5f58692d15b319de8a23a92045ada487
EC2 Default User <ec2-user@ip-172-31-39-164.ec2.internal>
2020-09-24 19:49:41 +0000

    Repo cleanup

--- Data-Structures/WeeklyAssignment3/test.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-const myArray = ['Apple', 'Banana', 'Pear']
-//when writing things
-fs.writeFileSync('WeeklyAssignment3/test.json', JSON.stringify(myArray));
-
-// when reading things
-JSON.parse()
